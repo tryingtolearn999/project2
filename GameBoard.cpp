f4f52903a15c10e63c4f0dd530fdc54da64dd10b
susmitha-chittem <susmitha.chittem@csu.fullerton.edu>
2016-11-07 15:20:00 -0800

    Update GameBoard.cpp

--- GameBoard.cpp
+++ GameBoard.cpp
@@ -12,6 +12,73 @@
 // Otherwise, player stays put at the landing square
 // TO DO: implement this method
 void GameBoard::buildBoard() {
+    for(int i = 0; i < 101; i++)
+   {
+     switch (i) 
+      {
+            case GameBoard::ONE: // Ladder
+                (*Space)[i] = 38;
+                break;
+            case GameBoard::FOUR: // Ladder
+                (*Space)[i] = 14;
+                break;
+            case GameBoard::NINE: // Ladder
+                (*Space)[i] = 31;
+                break;
+            case GameBoard::SIXTEEN: // Chute
+                (*Space)[i] = 6;
+                break;
+            case GameBoard::TWENTY_ONE: // Ladder
+                (*Space)[i] = 42;
+                break;
+            case GameBoard::TWENTY_EIGHT: // Ladder
+                (*Space)[i] = 84;
+                break;
+            case GameBoard::THIRTY_SIX: // Ladder
+                (*Space)[i] = 44;
+                break;
+            case GameBoard::FORTY_SEVEN: // Chute
+                (*Space)[i] = 26;
+                break;
+            case GameBoard::FORTY_NINE: // Chute
+                (*Space)[i] = 11;
+                break;
+            case GameBoard::FIFTY_ONE: // Ladder
+                (*Space)[i] = 67;
+                break;
+            case GameBoard::SIXTY_TWO: // Chute
+                (*Space)[i] = 19;
+                break;
+            case GameBoard::FIFTY_SIX: // Chute
+                (*Space)[i] = 53;
+                break;
+            case GameBoard::SIXTY_FOUR: // Chute
+                (*Space)[i] = 60;
+                break;
+            case GameBoard::SEVENTY_ONE: // Ladder
+                (*Space)[i] = 91;
+                break;
+            case GameBoard::EIGHTY: // Ladder
+                (*Space)[i] = 100;
+                break;
+            case GameBoard::EIGHTY_SEVEN: // Chute
+                (*Space)[i] = 24;
+                break;
+            case GameBoard::NINETY_THREE: // Chute
+                (*Space)[i] = 73;
+                break;
+            case GameBoard::NINETY_FIVE: // Chute
+                (*Space)[i] = 75;
+                break;
+            case GameBoard::NINETY_EIGHT: // Chute
+                (*Space)[i] = 78;
+                break;
+            default:
+                (*Space)[i] = i;
+                break;
+        }
+   }
    // TODO: implement this function properly
    throw std::logic_error("not implemented yet");
+   
 }
